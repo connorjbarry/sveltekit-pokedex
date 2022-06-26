eae8017792e0c8eab271dd9e527a320312ff6ead
connorjbarry <barry31@purdue.edu>
2022-06-26 18:03:38 -0500

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,38 @@
+<script>
+	import PokemanCard from '../components/pokemanCard.svelte';
+	import { pokemon } from '../stores/pokestore';
+
+	let searchTerm = '';
+	let filteredPokemon = [];
+
+	$: {
+		console.log(searchTerm);
+		if (searchTerm) {
+			// search pokemon
+			filteredPokemon = $pokemon.filter((pokeman) =>
+				pokeman.name.toLowerCase().includes(searchTerm.toLowerCase())
+			);
+		} else {
+			filteredPokemon = [...$pokemon];
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
+
+<input
+	type="text"
+	placeholder="Search Pokemon"
+	class=" w-full rounded-md text-lg p-4 border-2 border-gray-200"
+	bind:value={searchTerm}
+/>
+
+<div class="grid gap-4 md:grid-cols-2 grid-cols-1 py-4">
+	{#each filteredPokemon as pokeman}
+		<PokemanCard {pokeman} />
+	{/each}
+</div>
